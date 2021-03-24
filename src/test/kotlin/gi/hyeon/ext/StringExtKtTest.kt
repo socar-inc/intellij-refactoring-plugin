8041c0447a9deda93e6e1c759a3a6c4b1f1c196d
Hyeon-Gi Jeon <hgijeon@gmail.com>
2021-03-24 21:54:36 +0900

    테스트코드 작성

---
+++ src/test/kotlin/gi/hyeon/ext/StringExtKtTest.kt
@@ -0,0 +1,15 @@
+package gi.hyeon.ext
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class StringExtKtTest {
+
+    @Test
+    fun snakeToLowerCamelCase() {
+        Assertions.assertEquals(
+            "code01WrittenBelow",
+            "code_01_written_below".snakeToLowerCamelCase()
+        )
+    }
+}
