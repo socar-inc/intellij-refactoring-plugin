71c05c64cd16fd933e0059c6c63edb7d0936a8a9
Hyeon-Gi Jeon <hgijeon@gmail.com>
2021-03-24 23:37:51 +0900

    액션 버튼 추가

---
+++ src/main/kotlin/gi/hyeon/intellij/plugin/actions/RefactorViewBinding.kt
@@ -0,0 +1,28 @@
+package gi.hyeon.intellij.plugin.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.ui.Messages
+
+class RefactorViewBinding : AnAction() {
+    override fun update(event: AnActionEvent) {
+        // Set the availability based on whether a project is open
+        event.presentation.isEnabledAndVisible = event.project != null
+    }
+
+    override fun actionPerformed(event: AnActionEvent) {
+        val currentElement = event.getData(CommonDataKeys.NAVIGATABLE)
+
+        Messages.showOkCancelDialog(
+            event.project,
+            "${event.presentation.text} 실행.\ncurrent element: $currentElement",
+            event.presentation.description,
+            "실행",
+            "취소",
+            Messages.getInformationIcon()
+        ).let {
+            if (it == Messages.OK) println("ok")
+        }
+    }
+}
